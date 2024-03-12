f6c9226a1de64da6919941db9040acc98f8be79c
Artem <tema.butkov.03@bk.ru>
2024-03-12 11:38:16 +0400

    some

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-
+import matplotlib.patches as ptc
+import matplotlib.patches as Patch
+import matplotlib.pyplot as pyplot
 
 
 
 def main():
-    print ("a")
+
+    circle = ptc.Circle((0.2,0.2), 0.1)
+    circle2 = ptc.Circle((0.4,0.2), 0.1)
+    circle3 = ptc.Circle((0.6,0.2), 0.1)
+
+
+    fig, ax = pyplot.subplots()
+    ax.add_patch(circle)
+    ax.add_patch(circle2)
+    ax.add_patch(circle3)
+
+    ax.set_aspect('equal')
+    pyplot.show()
 
 
 
